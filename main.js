8f8fd8482ed321ef0dbc1968430dc4529e851985
hoonsbory <tonymkcv@naver.com>
2023-02-03 14:55:22 +0900

    fist commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const audio = document.getElementById('audio');
+const resultElem = document.getElementById('result');
+const date = document.getElementById('date');
+const getData = async () => {
+  try {
+    const data = await axios.get(
+      'https://data.sec.gov/submissions/CIK0001418100.json',
+    );
+    const result = data.data.filings.recent.form[0];
+    const dateObj = new Date();
+    resultElem.innerText = result;
+    date.innerText = `${
+      dateObj.getMonth() + 1
+    }월 ${dateObj.getDate()}일 ${dateObj.getHours()}시 ${dateObj.getMinutes()}분 ${dateObj.getSeconds()}초`;
+    if (!result.includes('13G')) {
+      audio.play();
+    } else {
+      setTimeout(() => {
+        getData();
+      }, 3000);
+    }
+  } catch (error) {
+    console.log(error);
+    getData();
+    return;
+  }
+};
+
+getData();
